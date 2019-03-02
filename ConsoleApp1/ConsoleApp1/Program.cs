ab5371345d0c5065f37f01c4f3f0deecd11787fd
김용찬 <ycbalor@gmail.com>
2019-03-02 14:40:19 +0900

    열거형

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,45 +6,45 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-	// Value Type
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			int a = 1;
-			int b = a;
-
-			Console.WriteLine(a);
-			Console.WriteLine(b);
-
-			b = 2;
-			Console.WriteLine(a);
-			Console.WriteLine(b);
-		}
-	}
-
-	//// Reference Type
-	//class RType
-	//{
-	//	public object value;
-	//}
-
+	//// Value Type
 	//class Program
 	//{
 	//	static void Main(string[] args)
 	//	{
-	//		RType a = new RType();
-	//		RType b = new RType();
+	//		int a = 1;
+	//		int b = a;
 
-	//		a.value = 1;
-	//		b = a; // a가 참조하는 것을 b도 참조한다.
+	//		Console.WriteLine(a);
+	//		Console.WriteLine(b);
 
-	//		Console.WriteLine(a.value);
-	//		Console.WriteLine(b.value);
-
-	//		a.value = 2;
-	//		Console.WriteLine(a.value);
-	//		Console.WriteLine(b.value);
+	//		b = 2;
+	//		Console.WriteLine(a);
+	//		Console.WriteLine(b);
 	//	}
 	//}
+
+	// Reference Type
+	class RType
+	{
+		public object value;
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RType a = new RType();
+			RType b = new RType();
+
+			a.value = 1;
+			b = a; // a가 참조하는 것을 b도 참조한다.
+
+			Console.WriteLine(a.value);
+			Console.WriteLine(b.value);
+
+			a.value = 2;
+			Console.WriteLine(a.value);
+			Console.WriteLine(b.value);
+		}
+	}
 }
---
+++ day1/day1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1
+{
+	enum Day1
+	{
+		Monday,
+		Thesday,
+		Wednesday,
+		Thursday,
+		Friday,
+		Saturday,
+		Sunday
+	}
+
+	enum Day2 : byte
+	{
+		Monday = 11,
+		Thesday, // 12
+		Wednesday, // 13
+		Thursday, // 14
+		Friday, // 15
+		Saturday, // 16
+		Sunday = Monday + 100
+	}
+
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Day1 whatDay = Day1.Sunday;
+
+			Console.WriteLine("{0}", whatDay); // Sunday
+			Console.WriteLine("{0}", (int)whatDay); // 6
+
+			whatDay = (Day1)6;
+			Console.WriteLine("{0}", whatDay); // Sunday
+
+			//Day2 whatDay2 = Day2.Monday;
+			//Console.WriteLine("{0}", whatDay2); // Monday
+			//Console.WriteLine("{0}", (byte)whatDay2); // 11
+
+			//whatDay2 = (Day2)111;
+			//Console.WriteLine("{0}", whatDay); // Sunday
+		}
+	}
+}
