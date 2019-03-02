83b09f2db8b03e4391bb3039a863c0441037edba
김용찬 <ycbalor@gmail.com>
2019-03-02 12:19:47 +0900

    연산자

---
+++ InteropServices/InteropServices/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices; // 마샬 사용하기 위해서
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteropServices
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int iSize = sizeof(int); // 인트형 몇바이트냐
+			Console.WriteLine("int형 바이트길이 : {0}", iSize);
+
+			Type myType = typeof(int); // 원형
+			Console.WriteLine("typeof(int) : {0}", myType);
+
+			int i = 10;
+			Type myType2 = i.GetType(); // 원형
+			Console.WriteLine("i.GetType() : {0}", myType2);
+
+			int j = 99; // 변수의 사이즈 궁금할 때,사용
+			Console.WriteLine("Size of j : {0}", Marshal.SizeOf(j));
+		}
+	}
+}
---
+++ MainArgTest/MainArgTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainArgTest
+{
+	class Program
+	{
+		// CLR이 Main을 호출할 때 인자를 넣어주는 테스트
+		static void Main(string[] args)
+		{
+			Console.WriteLine(args[0]);
+			Console.WriteLine(args[1]);
+		}
+	}
+}
