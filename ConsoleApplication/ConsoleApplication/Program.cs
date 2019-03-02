18b0ae77a720bd29c16b7a0583317aaed6620f15
김용찬 <ycbalor@gmail.com>
2019-03-02 17:00:47 +0900

    가변길이 매개변수

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+	class Program
+	{
+		// params 키워드를 지정하여 개수가 가변적인 매개변수를 메소드에 전달할 수 있다.
+		private static int Sum(string title, params int[] num)
+		{
+			int sum = 0;
+			Console.WriteLine("Sum of {0}", title);
+
+			// 데이터 들어있는 쪽이 오른쪽, 왼쪽에는 데이터 타입
+			foreach (int i in num)
+			{
+				sum += i;
+			}
+
+			Console.WriteLine("Sum :: {0}\n", sum);
+			return sum;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Sum("Test1", 1, 2));
+			Console.WriteLine(Sum("Test2", 1, 2, 3));
+			Console.WriteLine(Sum("Test3", 1, 2, 3, 4));
+		}
+	}
+}
---
+++ Test0302/Test0302/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Test0302
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			Console.WriteLine("숫자 입력(구분값 ,):");
+//			string str = Console.ReadLine();
+//			str.Trim();
+//			string[] strarr = str.Split(',');
+
+//			for (int i = 0; i < strarr.Length; i++)
+//			{
+//				Console.Write(strarr[i] + " ");
+//			}
+//		}
+//	}
+//}
+//namespace Test0302
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			Console.WriteLine("입력숫자:");
+//			string str = Console.ReadLine();
+
+//			int num = Convert.ToInt32(str);
+//			int sum = 0;
+
+
+//			Console.WriteLine("{0}까지의 숫자: ", num);
+
+//			for(int i = 1; i <= num; i++)
+//			{
+//				sum += i;
+//				Console.Write(i + " ");
+//			}
+//			Console.WriteLine();
+
+//			Console.WriteLine("{0}까지 숫자의 합은: {1}", num, sum);
+//		}
+//	}
+//}
+
+//namespace Test0302
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int[] num = new int[10];
+//			int sum = 0;
+//			double average = 0;
+//			for (int i = 0; i < 10; i++)
+//			{
+//				num[i] = Convert.ToInt32(Console.ReadLine());
+//				Console.WriteLine("숫자-{0}: {1}", i + 1, num[i]);
+//				sum += num[i];
+//				average = sum / 10.0;
+//			}
+//			Console.WriteLine("합: {0}", sum);
+//			Console.WriteLine("평균: {0}", average);
+//		}
+//	}
+//}
+
+
+namespace Test0302
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("출력을 원하는 구구단 단수:");
+			string str = Console.ReadLine();
+			int num = Convert.ToInt32(str);
+
+			for (int i = 1; i <= num; i++)
+			{
+				for (int j = 1; j <= 9; j++)
+				{
+					Console.WriteLine("{0} x {1} = {2}", i, j, i * j);
+				}
+			}
+		}
+	}
+}
