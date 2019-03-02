7752ee7b96cf3e7a16471a679851ec8625268f95
김용찬 <ycbalor@gmail.com>
2019-03-02 13:47:01 +0900

    연산

---
+++ OperationTest/OperationTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(+ 3);
+			Console.WriteLine(3 + 3);
+			Console.WriteLine(3 + .3);
+			Console.WriteLine("3" + "3");
+			Console.WriteLine(3.0 + "3");
+
+
+			Console.WriteLine(!true);
+
+			byte x = 10;
+
+			Console.WriteLine(~x);
+
+			Console.WriteLine(unchecked((short)50000));
+		}
+	}
+}
