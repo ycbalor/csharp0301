1533bef0d1eeaac5d8df9387c8b17017f5df959a
김용찬 <ycbalor@gmail.com>
2019-03-02 15:47:20 +0900

    반복문

---
+++ WhileTest/WhileTest/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("숫자를 입력하세요:");
+			string str = Console.ReadLine();
+			int m = 0;
+
+			bool parsed = Int32.TryParse(str, out m);
+
+			if (!parsed)
+			{
+				Console.WriteLine("숫자를 제대로 입력하세요.");
+				Environment.Exit(0);
+			}
+
+			int i = 0, sum = 0;
+
+			while (i <= m)
+			{
+				if (i % 2 == 0)
+				{
+					sum += i;
+				}
+				i++;
+			}
+			Console.WriteLine("1부터 {0}까지의 짝수합 {1}", m, sum);
+
+
+			i = 0;
+			sum = 0;
+
+			do
+			{
+				if (i % 2 == 0)
+				{
+					sum += i;
+				}
+				i++;
+			} while (i <= m);
+			Console.WriteLine("1부터 {0}까지의 짝수합 {1}", m, sum);
+
+			sum = 0;
+
+			for (i = 0; i <= m; i++)
+			{
+				if (i % 2 == 0)
+				{
+					sum += i;
+				}
+			}
+			Console.WriteLine("1부터 {0}까지의 짝수합 {1}", m, sum);
+		}
+	}
+}
