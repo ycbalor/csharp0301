951f2aae4a70c712cab914676a2ae134107e3c5e
김용찬 <ycbalor@gmail.com>
2019-03-02 11:57:34 +0900

    형변환

--- TypeCastExample1/TypeCastExample1/Program.cs
+++ TypeCastExample1/TypeCastExample1/Program.cs
@@ -11,12 +11,20 @@ namespace TypeCastExample1
 		static void Main(string[] args)
 		{
 			short a = 10;
-			int b = a; // short은 int형에 묵시적으로 변경된다.
+			int b = a; // short은 int형에 묵시적으로 변경된다. (int는 4바이트)
 			int c = 50000;  // 이진수로 0000 0000 0000 0000 1100 0011 0101 0000이며
 							// Short형으로 변환하면 부호비트가 0에서 1로, 음수가 된다.
 
-			short d = (short)c; // 명시적으로 형 변환 하지않으면 오류
-			Console.WriteLine("Short:{0}", d);
+			try
+			{
+				// 형변환 해줄 때는, checked를 해주는 것이 좋다. 오버플로우 검사 키워드.
+				short d = checked((short)c); // 명시적으로 형 변환 하지않으면 오류
+				Console.WriteLine("Short:{0}", d);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("예외: {0}", e.StackTrace);
+			}
 		}
 	}
 }
