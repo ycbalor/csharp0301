1234257307199aad5e7cb993e9c93d5d8f2fc0ca
김용찬 <ycbalor@gmail.com>
2019-03-03 09:59:09 +0900

    dll 테스트

---
+++ Shapes/Shapes/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+	public class Circle
+	{
+		double radius;
+
+		public Circle()
+		{
+			radius = 0;
+		}
+
+		public Circle(double radius)
+		{
+			this.radius = radius;
+		}
+
+		public double Area()
+		{
+			return Math.PI * (radius * radius);
+		}
+
+		public void Draw()
+		{
+			Pen p = new Pen(Color.Red);
+		}
+    }
+}
